7d66fbad7fe3ee123b2fdf9637b7f1b66ec7acfe
matt venn <matt@mattvenn.net>
2017-04-03 17:14:19 +0200

    fetch csv

--- software/account.py
+++ software/account.py
@@ -21,73 +21,35 @@ parser.add_argument('--fetch', action='store_const', const=True)
 parser.add_argument('--hack', action='store_const', const=True) # for old firmware
 args = parser.parse_args()
 
-filter = 'gt[timestamp]=20170201' 
-url = 'http://phant.cursivedata.co.uk/output/%s.json?%s' % (key, filter)
+cutting = False
+stopped = False
+current_user = None
+total_mins = 0
+month = 4
+
+filter = 'gt[timestamp]=2017%02d01&lt[timestamp]=2017%02d01' % (month - 1, month)
+url = 'http://phant.cursivedata.co.uk/output/%s.csv?%s' % (key, filter)
 if args.fetch:
     print("fetching")
     r = requests.get(url)
-    records = r.json()
-    with open("rec.json",'w') as fh:
-        json.dump(records, fh)
-else:
-    print("loading cached data")
-    with open("rec.json") as fh:
-        records = json.load(fh)
+    with open("rec.csv",'w') as fh:
+        fh.write(r.text)
     
-print("got %d records from %s to %s" % (len(records), records[-1]['timestamp'], records[0]['timestamp']))
-total_mins = 0
+#print("got %d records from %s to %s" % (len(records), records[-1]['timestamp'], records[0]['timestamp']))
 
-cutting = False
-stopped = False
-prev_record = None
-current_user = None
 
-for record in records:
-#    print(record["rfid"], record['laser'], record['timestamp'])
-    # start state
-    if not cutting and record['laser'] == '1':
-        for user in users:
-            if user['rfid'] == record['rfid']:
-                cutting = True
-                start_time = record['timestamp']
-                current_user = user
-                #print("session started for %s at %s" % (current_user['name'], start_time))
-        if current_user == None:
-            print("no such user")
+with open("rec.csv",'r') as fh:
+    reader = csv.DictReader(fh, delimiter=',')
+    for record in reader:
 
-    if cutting and (record['rfid'] != current_user['rfid']):
-        if record['rfid'] != "":
-            print("error")
+        # end state
+        if record['laser'] == '1':
+            for user in users:
+                if user['rfid'] == record['rfid']:
+                    current_user = user
+            current_user['minutes'] += 1
+            total_mins += 1
 
-    # hack for non finished records (remove after Feb)
-    end = datetime.strptime(record['timestamp'], "%Y-%m-%dT%H:%M:%S.%fZ")
-    if prev_record:
-        prev_record_end = datetime.strptime(prev_record['timestamp'], "%Y-%m-%dT%H:%M:%S.%fZ")
-        delta = prev_record_end - end
-        minutes = int(delta.total_seconds() / 60)
-        if cutting and minutes > 5:
-            print minutes
-            end = prev_record_end
-            stopped = True
-
-    # end state
-    if cutting and record['laser'] == '0':
-        stopped = True
-
-    if stopped:
-        #2017-01-16T14:42:22.852Z
-        start = datetime.strptime(start_time, "%Y-%m-%dT%H:%M:%S.%fZ")
-
-        delta = start - end
-        minutes = int(delta.total_seconds() / 60)
-        current_user['minutes'] += minutes
-        print("session for %10s started at %s was %d minutes" % (current_user['name'], start_time, minutes))
-        
-        current_user = None
-        cutting = False
-        stopped = False
-
-    prev_record = record
 
 for user in users:
     print("%20s : %4d = %.2f euro" % (user['name'], user['minutes'], user['minutes'] * cost_per_minute))
