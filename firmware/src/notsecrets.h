6fc581200487f38aac65faa6fe6f0da4c9d4ef60
Matt Venn <matt@mattvenn.net>
2021-03-17 18:06:06 +0100

    template

---
+++ firmware/src/notsecrets.h
@@ -0,0 +1,6 @@
+const char* ssid     = "";
+const char* password = "";
+const char* host = "phant.cursivedata.co.uk";
+const char* user_host = "mattvenn.net";
+const char* streamId   = "";
+const char* privateKey = "";
